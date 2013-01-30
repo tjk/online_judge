b9732a9b67a6ea5e4efb46bb760ba5a5abc40c6e
TJ Koblentz <tj.koblentz@gmail.com>
2013-01-29 19:21:43 -0800

    initial commit, 3n_+_1 accepted

---
+++ 100-3n_+_1/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <utility>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+
+using namespace std;
+
+static map<int,int> ccs;
+
+int cc(int n)
+{
+    int _cc;
+    if ((_cc = (*ccs.find(n)).second))
+        return _cc;
+    if (n % 2 == 1) {
+        _cc = 1 + cc(3 * n + 1);
+    } else {
+        _cc = 1 + cc(n / 2);
+    }
+    ccs.insert(mp(n, _cc));
+    return _cc;
+}
+
+int main()
+{
+    int _i, _j, i, j, k, cl, max;
+    ccs.insert(mp(1, 1));
+    while (cin >> _i) {
+        cin >> _j;
+        max = 0;
+        if (_i > _j) {
+            i = _j;
+            j = _i;
+        } else {
+            i = _i;
+            j = _j;
+        }
+        for (k = i; k <= j; k++) {
+            cl = cc(k);
+            if (cl > max)
+                max = cl;
+        }
+        cout << _i << " " << _j << " " << max << "\n";
+    }
+    return 0;
+}
