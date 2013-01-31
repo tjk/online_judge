853710a2bf56defd5d424aac3722717cf8bb581e
TJ Koblentz <tj.koblentz@gmail.com>
2013-01-30 19:53:47 -0800

    a little cleanup

--- 100-3n_+_1/main.cpp
+++ 100-3n_+_1/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <utility>
 #include <map>
-#include <vector>
-
-#define mp make_pair
 
 using namespace std;
 
@@ -12,21 +8,21 @@ map<int,int> ccs;
 int cc(int n)
 {
     int _cc;
-    if ((_cc = (*ccs.find(n)).second))
+    if ((_cc = ccs[n]))
         return _cc;
     if (n % 2 == 1) {
         _cc = 1 + cc(3 * n + 1);
     } else {
         _cc = 1 + cc(n / 2);
     }
-    ccs.insert(mp(n, _cc));
+    ccs[n] = _cc;
     return _cc;
 }
 
 int main()
 {
     int _i, _j, i, j, k, cl, max;
-    ccs.insert(mp(1, 1));
+    ccs[1] = 1;
     while (cin >> _i) {
         cin >> _j;
         max = 0;
